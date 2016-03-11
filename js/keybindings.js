e07c357ed2167832ce1ede4e2e3566a5f7d9a25f
Sam Lanning <sam@samlanning.com>
2016-03-11 10:24:48 +0000

    Fix not being able to type "w"

--- js/keybindings.js
+++ js/keybindings.js
@@ -6,21 +6,25 @@ define(['constants', 'storage','tabs'], function(C, storage, tabs){
     $(window).keydown(function(e){
       switch(e.which){
         case C.KEYCODES.T:
-          if(e.ctrlKey)
+          if (e.ctrlKey) {
             tabs.open_new_tab();
+          }
           break;
         case C.KEYCODES.Q:
-          if(e.ctrlKey)
+          if (e.ctrlKey) {
             storage.clear();
+          }
           break;
         case C.KEYCODES.W:
-          if(e.ctrlKey)
+          if (e.ctrlKey) {
             console.log("close window key");
             e.preventDefault();
+          }
           break;
         case C.KEYCODES.L:
-          if(e.ctrlKey)
+          if(e.ctrlKey) {
             tabs.focus_address_bar();
+          }
           break;
         default:
           console.log(e);
